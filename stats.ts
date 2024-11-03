38c929ad0e1ee4a5d86d3c577f3365adf28d92ac
yogi nugroho <Ugroho@proton.me>
2024-11-03 22:57:30 +0700

    not completed

--- stats.ts
+++ stats.ts
@@ -1,29 +1,37 @@
-async function checkServer(data: any, serverId: string | null) {
-    const selectedServer = data.servers.find((s: any) => s.serverId === serverId)
-    return selectedServer
+interface IStats {
+    checkStatsServer(): Promise<Boolean>
 }
-export async function checkStatsServer(): Promise<Boolean> {
-    const path = "./server.json";
-    const file = Bun.file(path);
+class Stats {
+    private async checkServer(data: any, serverId: string | null) {
+        const selectedServer = data.servers.find((s: any) => s.serverId === serverId)
+        return selectedServer
+    }
+    async checkStatsServer(): Promise<Boolean> {
+        const path = "./server.json";
+        const file = Bun.file(path);
 
-    // Muat file JSON
-    const servers = await file.json();
-
-
-    const serverIds = servers.servers.map((s: any) => s.serverId);
-    console.log("Server Avaible:", serverIds.join(", "));
-
-    const inputServer = prompt(`Please choose a server:`)
-
-    const selectedServer = await checkServer(servers, inputServer)
+        // Muat file JSON
+        const servers = await file.json();
 
 
-    if (!selectedServer) {
-        console.log(`Server ${inputServer} not found.`);
-        return false;
+        const serverIds = servers.servers.map((s: any) => s.serverId);
+        console.log("Server Avaible:", serverIds.join(", "));
+
+        const inputServer = prompt(`Please choose a server:`)
+
+        const selectedServer = await this.checkServer(servers, inputServer)
+
+
+        if (!selectedServer) {
+            console.log(`Server ${inputServer} not found.`);
+            return false;
+
+        }
+        console.log(`Server ${inputServer} found:`, selectedServer);
+        return true;
 
     }
-    console.log(`Server ${inputServer} found:`, selectedServer);
-    return true;
-
 }
+
+const stats = new Stats();
+stats.checkStatsServer();
